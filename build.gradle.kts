cbb24d530cd6b336a08bc6a496d01a7643d9d0e6
Petr Kadlec <petr.kadlec@finshape.com>
2025-07-23 22:09:44 +0200

    fixing tests

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,3 +5,6 @@
  * Learn more about Gradle by exploring our Samples at https://docs.gradle.org/8.10/samples
  * This project uses @Incubating APIs which are subject to change.
  */
+ task("test") {
+    println("Running test task...")
+ }
